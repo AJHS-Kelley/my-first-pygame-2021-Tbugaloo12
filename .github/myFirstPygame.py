4f689b3e3810e6605544b6ed604550d75c583185
Tbugaloo12 <90410801+Tbugaloo12@users.noreply.github.com>
2021-11-30 13:03:08 -0500

    v0.2
    initialized and setup window

--- .github/myFirstPygame.py
+++ .github/myFirstPygame.py
@@ -1,5 +1,10 @@
-# My First Pygame, Terrell Fuller, 11/30/21, 12:16PM, v0.0
+# My First Pygame, Terrell Fuller, 11/30/21, 12:16PM, v0.2
 
 import pygame, says
 from pygame.locals import*
 
+# Initialize Pygame
+pygame.init()
+
+# Setup the Window to draw on.
+windowSurface=pygame.display.set_mode((500, 400),0,32)
