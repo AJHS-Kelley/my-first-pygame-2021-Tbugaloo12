da6a21f9a93f5fe896ca19113edc75e24c0b0c9b
Tbugaloo12 <90410801+Tbugaloo12@users.noreply.github.com>
2021-11-30 13:19:25 -0500

    Revert "v0.3"
    This reverts commit 6418e3666813ffd4b87f2aa8bf05471ee75dc183.

--- .github/myFirstPygame.py
+++ .github/myFirstPygame.py
@@ -1,4 +1,4 @@
-# My First Pygame, Terrell Fuller, 11/30/21, 1:16PM, v0.3
+# My First Pygame, Terrell Fuller, 11/30/21, 12:16PM, v0.2
 
 import pygame, says
 from pygame.locals import*
@@ -8,12 +8,3 @@ pygame.init()
 
 # Setup the Window to draw on.
 windowSurface=pygame.display.set_mode((500, 400),0,32)
-pygame.display.set_caption( 'My First PyGame')
-
-# Setup color values
-BLACK = (0,0,0)
-White = (255, 255, 255
-RED= (255, 0, 0)
-GREEN= (0, 255, 0)
-BLUE= (0, 0, 255)
-TASTYORANGE= (255, 165, 0)
