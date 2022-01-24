ef8d74d10c3f0ffd915993bdded251da08dde7b7
Terrell. F <90410801+Tbugaloo12@users.noreply.github.com>
2022-01-24 12:02:56 -0500

    v0.3
    Created colors

--- .github/collision_detection.py
+++ .github/collision_detection.py
@@ -1,4 +1,4 @@
-# PyGame Collision Detection Practice, Terrell Fuller, January 24, 2022, 11:46, v0.2
+# PyGame Collision Detection Practice, Terrell Fuller, January 24, 2022, 12:01, v0.3
 
 import pygame, sys, random
 from pygame.locals import *
@@ -12,3 +12,9 @@ WINDOWWITH = 400
 WINDOWHEIGHT = 400
 windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDHEIGHT), 0, 32)
 pygame.display.set_caption('Collision Detection 2022')
+
+# Setup colors.
+BLACK = (0,0, 0)
+GREEN = (0, 255, 0)
+WHITE = (255, 255, 255)
+
