47d24de13cf2644604568ded54706cd9c7ef56a2
Terrell. F <90410801+Tbugaloo12@users.noreply.github.com>
2022-01-24 11:51:59 -0500

    v0.2
    Setup Pygame Window.

--- .github/collision_detection.py
+++ .github/collision_detection.py
@@ -1,1 +1,14 @@
-# PyGame Collision Detection Practice, Terrell Fuller, January 12, 2020, 11:46, v0.0
+# PyGame Collision Detection Practice, Terrell Fuller, January 24, 2022, 11:46, v0.2
+
+import pygame, sys, random
+from pygame.locals import *
+
+# Setup Pygame
+pygame.init()
+mainClock = pygame.time.Clock()
+
+# Setup the Pygame Window
+WINDOWWITH = 400
+WINDOWHEIGHT = 400
+windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDHEIGHT), 0, 32)
+pygame.display.set_caption('Collision Detection 2022')
